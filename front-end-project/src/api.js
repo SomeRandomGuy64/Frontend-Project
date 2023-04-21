53ca89c3d585cd5637913b2f05dccfefe8551bf9
somerandomguy64 <faizan163123@gmail.com>
2023-04-21 09:55:59 +0100

    task 7 done

--- front-end-project/src/api.js
+++ front-end-project/src/api.js
@@ -9,13 +9,33 @@ export const fetchReviews = () => {
 };
 
 export const fetchReviewById = (review_id) => {
-    return axios.get(`${baseUrl}reviews/${review_id}`).then(({data}) => {
-        return data.review;
-    })
-}
+  return axios.get(`${baseUrl}reviews/${review_id}`).then(({ data }) => {
+    return data.review;
+  });
+};
 
 export const fetchCommentsByReviewId = (review_id) => {
-  return axios.get(`${baseUrl}reviews/${review_id}/comments`).then(({data}) => {
-    return data.comments;
-  })
+  return axios
+    .get(`${baseUrl}reviews/${review_id}/comments`)
+    .then(({ data }) => {
+      return data.comments;
+    });
+};
+
+export const patchVotes = (review_id) => {
+  return axios
+    .patch(`${baseUrl}reviews/${review_id}`, { inc_votes: 1 })
+    .then(({ data }) => {
+      console.log(data);
+      return data;
+    });
+};
+
+export const patchDownVotes = (review_id) => {
+  return axios
+    .patch(`${baseUrl}reviews/${review_id}`, { inc_votes: -1 })
+    .then(({ data }) => {
+      console.log(data);
+      return data;
+    });
 }
--- front-end-project/src/components/Review.jsx
+++ front-end-project/src/components/Review.jsx
@@ -5,6 +5,9 @@ import Comments from "./Comment.jsx";
 
 function Review() {
   const { review_id } = useParams();
+  const [localVotes, setLocalVotes] = useState(0);
+  const [disableUpButton, setDisableUpButton] = useState(false);
+  const [disableDownButton, setDisableDownButton] = useState(false);
   const [review, setReview] = useState({});
   const [isLoading, setIsLoading] = useState({});
 
@@ -22,6 +25,24 @@ function Review() {
     return <p>review not found</p>;
   }
 
+  const handleUpvote = (review_id) => {
+    setDisableUpButton(true);
+    setDisableDownButton(false);
+    setLocalVotes((currVotes) => {
+      return currVotes + 1
+    })
+    api.patchVotes(review_id);
+  }
+
+  const handleDownvote = (review_id) => {
+    setDisableDownButton(true);
+    setDisableUpButton(false);
+    setLocalVotes((currVotes) => {
+      return currVotes - 1
+    })
+    api.patchDownVotes(review_id);
+  }
+
   return (
     <div className="Review">
       <h2>{review.title}</h2>
@@ -31,7 +52,9 @@ function Review() {
       <p>{review.review_body}</p>
       <img src={review.review_img_url} alt={review.title} />
       <p>Created at: {review.created_at}</p>
-      <p>Votes: {review.votes}</p>
+      <p>Votes: {review.votes + localVotes}</p>
+      <button disabled={disableUpButton} onClick={() => {handleUpvote(review.review_id)}}>Upvote!</button>
+      <button disabled={disableDownButton} onClick={() => {handleDownvote(review.review_id)}}>Downvote!</button>
       <p>Comment count: {review.comment_count}</p>
       <br></br>
       <h3>Comments: </h3>
